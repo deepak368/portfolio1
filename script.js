6e7e33882996e5919d0a00f154c71de37bb6d6b6
Deepak <deepakdinesh368@gmail.com>
2024-10-07 22:17:25 +0530

    created new repo to fix the error in the pull request

--- script.js
+++ script.js
@@ -54,7 +54,7 @@ ScrollReveal().reveal('.home-content p , .about-content', { origin:'right' });
 
 // typed js 
 const typed = new Typed('.multiple-text', {
-    strings: ['Django developer', 'Btech graduate','Diploma graduate'],
+    strings: ['Python developer', 'Btech graduate','Diploma graduate'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
